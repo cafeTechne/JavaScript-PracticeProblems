121319b43bbd3053da5ad247d90aff8033b3fe61
Gino <cafetechne@gmail.com>
2018-04-14 21:50:01 -0400

    more problems!

---
+++ FreeCodeCampProblems/BasicAlgorithmScripting/falsyBouncer.js
@@ -0,0 +1,14 @@
+
+function bouncer(arr) {
+  // Don't show a false ID to this bouncer.
+  
+  
+function isFalsy(value) {
+  return value;
+}
+
+    return arr.filter(isFalsy);
+}
+     
+bouncer([7, "ate", "", false, 9]);
+
